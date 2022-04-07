dda7b5883e47c35fb4dd053187afdabb5a1cc66b
titular33 <amorim.pedro@engenharia.ufjf.br>
2022-04-06 23:27:50 -0300

    PWA pop up

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import App from './App';
+
+let deferredPrompt; // Allows to show the install prompt
+let setupButton;
+window.addEventListener('beforeinstallprompt', (e) => {
+    // Prevent Chrome 67 and earlier from automatically showing the prompt
+    e.preventDefault();
+    // Stash the event so it can be triggered later.
+    deferredPrompt = e;
+    console.log("beforeinstallprompt fired");
+    if (setupButton == undefined) {
+        setupButton = document.getElementById("setup_button");
+    }
+    // Show the setup button
+    setupButton.style.display = "inline";
+    setupButton.disabled = false;
+});
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
